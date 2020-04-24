1211c364c972e3cda699b60e49fc9b67c1cb091c
Patrick White <patrick.cf.white@gmail.com>
2020-04-24 10:34:15 +0100

    ready for deployment

--- project/settings.py
+++ project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -138,3 +138,5 @@ REST_FRAMEWORK = {
 }
 
 AUTH_USER_MODEL = 'fretbored.User'
+
+django_heroku.settings(locals()) # put this last
